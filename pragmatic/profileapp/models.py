893c04b1c1744a03d6209b645d523ed8885920b8
엄상욱 <sanguookmax@gmail.com>
2021-07-15 14:28:43 +0000

    30강

--- pragmatic/profileapp/models.py
+++ pragmatic/profileapp/models.py
@@ -8,7 +8,7 @@ from django.contrib.auth.models import User
 
 class Profile(models.Model):
     # on_delete : 유저 삭제시 취할 행동, CASCADE: 유저 삭제시 프로파일 같이 삭제
-    user = models.OnToOneField(User, on_delete=models.CASCADE, related_name='profile')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, related_name='profile')
 
     image = models.ImageField(upload_to='profile/', null=True)
     nickname = models.CharField(max_length=20, unique=True, null=True)
