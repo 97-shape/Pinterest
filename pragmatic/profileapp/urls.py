8f32315fcd11aac451e981e510798662d9227d25
엄상욱 <sanguookmax@gmail.com>
2021-07-16 13:33:06 +0000

    30강

--- pragmatic/profileapp/urls.py
+++ pragmatic/profileapp/urls.py
@@ -1,6 +1,11 @@
+from django.urls import path
+
+# 31강
+from profileapp.views import ProfileCreateView
+
 
 app_name = 'profileapp'
 
-urlpatterns =[
-    
+urlpatterns = [
+    path('create/', ProfileCreateView.as_view(), name='create'),
 ]
--- pragmatic/profileapp/views.py
+++ pragmatic/profileapp/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.urls import reverse_lazy
+
+# 31강
+
+from django.views.generic import CreateView
+
+from profileapp.forms import ProfileCreationForm
+from profileapp.models import Profile
 
 # Create your views here.
+
+class ProfileCreateView(CreateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/create.html'
