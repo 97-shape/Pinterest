fb74705db44e8817fa678948b79abca40e194417
엄상욱 <sanguookmax@gmail.com>
2021-07-16 14:19:23 +0000

    32강

--- pragmatic/profileapp/urls.py
+++ pragmatic/profileapp/urls.py
@@ -2,10 +2,13 @@ from django.urls import path
 
 # 31강
 from profileapp.views import ProfileCreateView
+# 32강
+from profileapp.views import ProfileUpdateView
 
 
 app_name = 'profileapp'
 
 urlpatterns = [
     path('create/', ProfileCreateView.as_view(), name='create'),
+    path('update/<int:pk>', ProfileUpdateView.as_view(), name='update'),
 ]
--- pragmatic/profileapp/views.py
+++ pragmatic/profileapp/views.py
@@ -8,6 +8,11 @@ from django.views.generic import CreateView
 from profileapp.forms import ProfileCreationForm
 from profileapp.models import Profile
 
+# 32강
+
+from django.views.generic import UpdateView
+from profileapp.decorators import profile_ownership_required
+
 # Create your views here.
 
 class ProfileCreateView(CreateView):
@@ -22,3 +27,12 @@ class ProfileCreateView(CreateView):
         temp_profile.user = self.request.user
         temp_profile.save()
         return super().form_valid(form)
+
+@method_decorator(profile_ownership_required, 'get')
+@method_decorator(profile_ownership_required, 'post')
+class ProfileUpdateView(UpdateView):
+    model = Profile
+    context_object_name = 'target_profile'
+    form_class = ProfileCreationForm
+    success_url = reverse_lazy('accountapp:hello_world')
+    template_name = 'profileapp/update.html'
